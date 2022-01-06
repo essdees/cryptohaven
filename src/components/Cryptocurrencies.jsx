8d416c04570cb8968a00c13ad0324461aad9f298
Snehdeep Singh <essdees27@gmail.com>
2022-01-07 02:18:32 +0530

    almost completed news page, alternate thumbnail issues

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -8,7 +8,7 @@ const Cryptocurrencies = ({simplified}) => {
     const count = simplified? 10 : 100;
     const { data: cryptosList, isFetching}= useGetCryptosQuery(count);
     const [cryptos, setCryptos]= useState(cryptosList?.data?.coins);
-    console.log(cryptos);
+    
 
     if(isFetching) return 'Loading...';
     return (
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
-const News = () => {
+
+const {Text,Title} =Typography;
+const {Option}=Select;
+const demoImage="https://cdn.pixabay.com/photo/2018/05/17/21/26/cryptocurrency-3409725_1280.jpg"
+
+const News = (simplified) => {
+    const {data:cryptoNews}= useGetCryptoNewsQuery({newsCategory:'Cryptocurrency', count: simplified ? 6:12})
+    
+    if(!cryptoNews?.value) return 'Loading...';
+
     return (
-        <div>
-            
-        </div>
+        <Row gutter={[ 24, 24]}>
+            {cryptoNews.value.map((news,i) => (
+                <Col xs={24} sm={12} lg={8} key={i}>
+               <Card hoverable className='news-card'>
+                   <a href={news.url} target="_blank" rel="noreferrer">
+                       <div className="news-image-container">
+
+                       <Title className="news-title" level={4}>{news.name}</Title>
+                       <img src={news?.image?.thumbnail?.contentUrl} alt='news'/>
+                       </div>
+                       <p>
+                           {news.description> 100?
+                           `${news.description.substring(0,100)}...`
+                        :news.description}
+                       </p>
+                   </a>
+               </Card>
+               </Col> 
+            ))}
+        </Row>
     )
 }
 
